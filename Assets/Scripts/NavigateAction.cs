3937fa652c52aff8dbca47f69efcb871a3bf0f85
Romain Lorente <romain.lorente@epsi.fr>
2022-11-28 16:24:43 +0100

    blocage des flèches de navigation

--- Assets/Scripts/NavigateAction.cs
+++ Assets/Scripts/NavigateAction.cs
@@ -4,17 +4,21 @@ public class NavigateAction : AbsAction
 {
     public GameObject destinationPoint; //Prend un objet comportant un point de navigation (donc qui possède un NavigationPoint) servant de destination
     public int showWithCurrentNavId; //Id du point de navigation pour lequel on affichera cet objet
+    public int progressNeeded; //Progression minimale nécessaire pour afficher la flèche
 
     NavigationManager navMgr;
+    ObjectiveManager objMgr;
 
     void Start()
     {
         navMgr = Constants.GetNavigationManager();
+        objMgr = Constants.GetObjectiveManager();
     }
 
     void Update()
     {
-        bool active = navMgr.GetCurrentNavigationId() == showWithCurrentNavId;
+        bool progressOk = objMgr.GetProgress() >= progressNeeded;
+        bool active = progressOk && navMgr.GetCurrentNavigationId() == showWithCurrentNavId;
         gameObject.GetComponent<MeshRenderer>().enabled = active;
         gameObject.GetComponent<MeshCollider>().enabled = active;
     }
